bfbf159e2c85cecf4fe5c5c9ec81d9cc09360d7f
Hyoin-kim <hi980506@naver.com>
2021-08-21 03:20:15 +0900

    [Feat] alarm 기능 구현중

---
+++ Alarm/alarm.js
@@ -0,0 +1,25 @@
+function showInput(){
+    document.getElementById("timeSettingWrap").style.display="block";
+    document.getElementById("timeSettingWrap").style.display="block";
+}
+
+let addBtn = document.getElementById("add-btn");
+    addBtn.addEventListener("click",function(e){
+    document.getElementById("timeSettingWrap").style.display="none";
+
+let addTime = document.getElementById("timeResult");
+let addHour = document.getElementById("hourResult");
+let addMinute = document.getElementById("minResult");
+showAlarms();
+});
+
+function showAlarms(){
+    let newAlarm = "";
+    newAlarm += `
+    
+    `
+
+}
+
+
+
