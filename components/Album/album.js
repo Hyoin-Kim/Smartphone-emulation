d8b25ae606793f8faef1d422a084c1f37075b8c0
Hyoin-kim <hi980506@naver.com>
2021-08-23 00:06:24 +0900

    [Feat] 전체적인 앨범 기능(가로스크롤, border등, 중앙정렬) 구현

---
+++ components/Album/album.js
@@ -0,0 +1,14 @@
+//해당 사진 클릭시 확대 및 border표시 함수
+function expandImage(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    expandImg.src = imgs.src;
+
+    document.querySelectorAll("#imageFile").forEach
+    (el => el.style.borderColor ="white");
+
+    if(expandImg.src===imgs.src){
+      imgs.style.borderColor = "red";
+    }
+    imgs.map
+    expandImg.parentElement.style.display = "block";
+  }
