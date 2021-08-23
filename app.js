ac1afa8b71446ac7f4929ce7c0a41748e55331e3
Hyoin-kim <hi980506@naver.com>
2021-08-23 17:48:43 +0900

    [Feat] 앱 Drag&Drop 로컬스토리지 저장 기능 구현

--- app.js
+++ app.js
@@ -1,53 +1,95 @@
-    function getPage(type) {
-      if(type === "alarm"){ 
+var alarmButton = '<button class="appButton" id="alarm" onclick="getPage(\'alarm\');" ondragstart="dragStart(event);" draggable="true" ondragend="dragEnd(event)">알람</button>';
+var memoButton = '<button class="appButton" id="memo" onclick="getPage(\'memo\');" ondragstart="dragStart(event);" draggable="true" ondragend="dragEnd(event)">메모</button>';
+var albumButton = '<button class="appButton" id="album" onclick="getPage(\'album\');" ondragstart="dragStart(event);" draggable="true" ondragend="dragEnd(event)">사진</button>';
+
+function getPage(type) {
+    localStorage.setItem("page",type);
+    if(type === "alarm"){ 
         var template = document.getElementById("template-alarm").innerHTML;
         document.getElementById("page-container").innerHTML = template;
         showAlarms();
-      }else if(type === "memo"){
+    }else if(type === "memo"){
         var template = document.getElementById("template-memo").innerHTML;
         document.getElementById("page-container").innerHTML = template;
         showMemos();
-      }else if(type === "album"){
+    }else if(type === "album"){
         var template = document.getElementById("template-album").innerHTML;
         document.getElementById("page-container").innerHTML = template;
-      }else if(type === "menu"){
+    }else if(type === "menu"){
         var template = document.getElementById("template-menu").innerHTML;
         document.getElementById("page-container").innerHTML = template;
+        checkMenuState();
+    }
+}
+
+//Drog & Drop 로컬스토리지 저장
+function checkMenuState(){
+    var menu = localStorage.getItem("menu");
+    if(menu === null || menu === undefined){ //로컬스토리지에 menu값이 null이거나 undefined일때
+        var menu = ["alarm","memo","album"];
+        localStorage.setItem("menu",JSON.stringify(menu));
+    }else{
+        menu = JSON.parse(menu);
+    }
+    menu.forEach(function(item){
+      if(item === "alarm"){
+          document.getElementById("menu-container").innerHTML += alarmButton;
+      }else if(item === "memo"){
+          document.getElementById("menu-container").innerHTML += memoButton;
+      }else if(item === "album"){
+          document.getElementById("menu-container").innerHTML += albumButton;
+      }
+    });
+}
+
+function checkPage(){
+    var page = localStorage.getItem("page");
+    if(page !== undefined && page !== null){
+        getPage(page);
+    }else{
+        getPage("menu");
+    }
+}
+
+function allowDrop() {
+    event.preventDefault();
+};
+      
+function dropItem() {
+    let targetEle = event.target;
+    let id = event.dataTransfer.getData('text');
+    let moveElement = document.getElementById(id);
+    let newMenu = [];
+  
+    if(moveElement.offsetLeft <= targetEle.offsetLeft){
+      //움직이고자 하는 아이콘이 왼쪽에 더 가까울때
+      if(targetEle.getAttribute("class") === "appButton"){
+          targetEle.after(moveElement);
+      }
+    }else{
+      //움직이고자 하는 아이콘이 오른쪽에 더 가까울때
+      if(targetEle.getAttribute("class") === "appButton"){
+          targetEle.before(moveElement);
       }
     }
 
-    function allowDrop() {
-        event.preventDefault();
-      };
+    document.querySelectorAll(".appButton").forEach
+    (el => newMenu.push(el.getAttribute("id")));
+    
+    localStorage.setItem("menu",JSON.stringify(newMenu));
+};
       
-      function dropItem() {
-        let targetEle = event.target;
-        let id = event.dataTransfer.getData('text');
-        let moveElement = document.getElementById(id);
-        targetEle.before(moveElement);
-      };
+function dragStart() {
+    let thisElement = event.target;
+    let thisId = thisElement.id;
+    thisElement.classList.add('is-dragging');
+    event.dataTransfer.setData('text/plain', thisId);
+};
       
-      function dragStart() {
-        let thisElement = event.target;
-        let thisId = thisElement.id;
-        thisElement.classList.add('is-dragging');
-        event.dataTransfer.setData('text/plain', thisId);
-      };
+function dragEnd() {
+    let thisElement = event.target;
+    thisElement.classList.remove('is-dragging');
+};
       
-      function dragEnd() {
-        let thisElement = event.target;
-        thisElement.classList.remove('is-dragging');
-      };
 
-      //드래그앤 드롭 로컬스토리지 저장
-      let drag = localStorage.getItem("appbutton");
-
-      if (drag===null) {
-        memosObj = [];
-        console.log("null",memosObj);
-      } else {
-        memosObj = JSON.parse(memos);
-        console.log(memosObj);
-      } 
-
-
+checkPage();
